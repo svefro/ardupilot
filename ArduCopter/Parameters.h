9ee277176ede55b41f64101ccaeea6bb96f09ca8
svefro <svefro@gmail.com>
2014-06-12 11:02:09 +0200

    STB_ANGLEBOOST Parameter

--- ArduCopter/Parameters.h
+++ ArduCopter/Parameters.h
@@ -115,6 +115,8 @@ public:
         k_param_serial0_baud,
         k_param_serial1_baud,
         k_param_serial2_baud,
+        
+        k_param_stb_angleboost = 56,         // 56
 
         // 65: AP_Limits Library
         k_param_limits = 65,            // deprecated - remove
@@ -383,6 +385,8 @@ public:
     AP_Int8         ch7_option;
     AP_Int8         ch8_option;
     AP_Int8         arming_check;
+    
+    AP_Int8         stb_angleboost;
 
 #if FRAME_CONFIG ==     HELI_FRAME
     // Heli
