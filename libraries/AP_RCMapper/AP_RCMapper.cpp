74927860ef0e18bd45d6c844854b564aa5957c74
svefro <svefro@gmail.com>
2015-05-27 13:38:01 +0200

    Added CH 5,6,7,8 to RC Map (Copter Only)

--- libraries/AP_RCMapper/AP_RCMapper.cpp
+++ libraries/AP_RCMapper/AP_RCMapper.cpp
@@ -35,7 +35,43 @@ const AP_Param::GroupInfo RCMapper::var_info[] PROGMEM = {
     // @Increment: 1
     // @User: Advanced
     AP_GROUPINFO("YAW",         3, RCMapper, _ch_yaw, 4),
+    
+#if defined (FRAME_CONFIG) // We are running APM:Copter
 
+    // @Param: FLIGHTMODE_5
+    // @DisplayName: Flight Mode channel/CH5
+    // @Description: Flight Mode channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Flight Mode is normally on channel 5, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("FLIGHTMODE_5",         4, RCMapper, _ch_flightmode_5, 5),
+
+    // @Param: TUNE_6
+    // @DisplayName: Tune channel/CH6
+    // @Description: Tune channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Tune (also known as CH6) is normally on channel 6, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("TUNE_6",         5, RCMapper, _ch_tune_6, 6),
+    
+    // @Param: AUX1_7
+    // @DisplayName: AUX1 channel/CH7 
+    // @Description: AUX1 channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Aux1 (also known as CH7) is normally on channel 7, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("AUX1_7",         6, RCMapper, _ch_aux1_7, 7),
+    
+    // @Param: AUX2_8
+    // @DisplayName: AUX2 channel/CH8
+    // @Description: AUX2 channel number. This is useful when you have a RC transmitter that can't change the channel order easily. Aux2 (also known as CH8) is normally on channel 8, but you can move it to any channel with this parameter.
+    // @Range: 1 8
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("AUX2_8",         7, RCMapper, _ch_aux2_8, 8),
+
+#endif
+    
     AP_GROUPEND
 };
 
--- libraries/AP_RCMapper/AP_RCMapper.h
+++ libraries/AP_RCMapper/AP_RCMapper.h
@@ -24,7 +24,23 @@ public:
 
     /// yaw - return input channel number for yaw / rudder input
     uint8_t yaw() const { return _ch_yaw; }
+    
+#if defined (FRAME_CONFIG) // We are running APM:Copter
 
+    /// flightmode
+    uint8_t flightmode_5() const { return _ch_flightmode_5; }
+    
+    /// tune
+    uint8_t tune_6() const { return _ch_tune_6; }
+    
+    /// aux1
+    uint8_t aux1_7() const { return _ch_aux1_7; }
+    
+    /// aux2
+    uint8_t aux2_8() const { return _ch_aux2_8; }
+
+#endif
+    
     static const struct AP_Param::GroupInfo var_info[];
 
 private:
@@ -33,5 +49,11 @@ private:
     AP_Int8 _ch_pitch;
     AP_Int8 _ch_yaw;
     AP_Int8 _ch_throttle;
+#if defined (FRAME_CONFIG) // We are running APM:Copter
+    AP_Int8 _ch_flightmode_5;
+    AP_Int8 _ch_tune_6;
+    AP_Int8 _ch_aux1_7;
+    AP_Int8 _ch_aux2_8;
+#endif
 };
 #endif
